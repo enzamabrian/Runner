059fa1f3a39d4faf1317c53ba558793c75c205c6
Brian <brianenzama@surepayltd.com>
2026-03-09 16:57:53 +0300

    Update

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://http://192.168.100.163:5000/api",
+  baseURL: "http://192.168.100.163:5000/api",
   headers: {
     "Content-Type": "application/json"
   }
